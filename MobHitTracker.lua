f5da8ff5a632723eee3ce3a28bbd1b0753ca7e7b
mlheur <mlheur@gmail.com>
2025-12-14 12:04:30 -0800

    docs: added comments on sequencing of a mob hit

--- MobHitTracker.lua
+++ MobHitTracker.lua
@@ -1,3 +1,38 @@
+-- A mob hit will have a variety of states depending on rRoll.sType == "attack" or "save"
+--  This is necessary because the attacker knows nothing about the save result, only the
+--  target (which is the source on the save roll with a nil target) has the save result; and
+--  because tracking requires knowing the iMobAttackID which is lost when transferring the
+--  roll source to victim to run the save; and because the damage roll knows nothing about the
+--  attack roll that started it all.
+--
+-- Mob hit values:
+--  integer 0: a mob hit has been instigated, does not matter if attack or save; the placeholder has been allocated.
+--  integer 1: all the hit/miss/save/fail results have been reported to chat.
+--  any string: the result of the attack, i.e. hit or miss; or the result of the save, i.e. success or failure
+--
+-- Sequencing: will change depending on attack or save roll.
+--
+--  On Attack Roll by instigator: start tracking hits, all results set to 0.
+--  On each "attack" roll by each mobber: we still have the iMobAttackID, just log the result ("hit"/"miss")
+--  After the last attack roll is accounted for, summarize hits, set all values to 1.
+--  On Damage roll by instigator: find once the iMobAttackID: the one whose value is 1.
+--   this is to add the iMobAttackID on all the damage submissions, so the upcoming
+--   damage results can be correlated back to a particular instigation, so __that__ attack
+--   can be wrapped up and summarized in a call to reportMobAttackComplete().
+--
+-- --OR--
+--
+--  On Powersave Roll by instigator: start tracking hits, all results set to 0.
+--  On each "save" roll by the target:
+--   find the attack ID for that mobber (will be 0), set the result ("success"/"failure")
+--  After all mobbers' saves are accounted for, summarize hits, set all values to one.  When the save
+--   has no damage component, e.g. Frightful Presence's save or be frightened; or if all damage
+--   components were avoided due to successful save; report no damage via call to reportMobAttackComplete()
+--     --OR--
+--  After all mobbers' saves are accounted for, summarize hits, set all values to one.  When the save
+--   has further damage components, e.g. fireball half-on-success ...
+
+
 function reset() self._hitTracker = {} end
 
 function dump(sMsg) MobManager.dump(sMsg .. " dump _hitTracker",self._hitTracker) end
